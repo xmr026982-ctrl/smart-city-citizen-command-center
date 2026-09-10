7c82ad4faeb71ddbd9fd8860d7d7fa0dcc7b9c81
Swarup Sutradhar <sutradharswarup2007@gmail.com>
2026-09-10 19:59:54 +0530

    Add map and internship location marker

---
+++ frontend/src/components/map/MapView.jsx
@@ -0,0 +1,59 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
+
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const defaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
+
+function MapView() {
+  const MAPTILER_KEY = import.meta.env.VITE_MAPTILER_KEY;
+
+  return (
+    <MapContainer
+      center={[22.5726, 88.3639]}
+      zoom={12}
+      style={{
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <TileLayer
+        attribution="&copy; MapTiler &copy; OpenStreetMap contributors"
+        url={`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${MAPTILER_KEY}`}
+      />
+
+      <Marker
+        position={[22.5726, 88.3639]}
+        icon={defaultIcon}
+      >
+      <Popup>
+         📍 You are here
+      </Popup>
+     </Marker>
+     <Marker
+        position={[22.57552106681359, 88.42715011092969]}
+        icon={defaultIcon}
+      >
+        <Popup>
+           💼 Internship Location
+        </Popup>
+     </Marker>
+    </MapContainer>
+  );
+}
+
+export default MapView;
